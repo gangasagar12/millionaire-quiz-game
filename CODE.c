7042c806720d2fe2705b83c49b394ce59ac8c167
Ganga Sagar Joshi <197456338+gangasagar12@users.noreply.github.com>
2025-06-25 15:08:54 +0545

    new updated

--- CODE.c
+++ CODE.c
@@ -11,3 +11,20 @@
 #define MAX_NAME_LEN 50
 #define SCOREBOARD_FILE "scoreboard.txt"
 #define MAX_SCORES 10
+
+const char* COLOR_END = "\033[0m";
+const char* RED = "\033[1;31m";
+const char* GREEN = "\033[1;32m";
+const char* YELLOW = "\033[1;33m";
+const char* BLUE = "\033[1;34m";
+const char* PINK = "\033[1;35m";
+const char* AQUA = "\033[1;36m";
+const char* WHITE = "\033[1;37m";
+
+typedef struct {
+    char text[MAX_QUES_LEN];
+    char options[4][MAX_OPTION_LEN];
+    char correct_option;
+    int timeout;
+    int prize_money;
+} Question;
